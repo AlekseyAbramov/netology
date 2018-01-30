6b2424d76830dcc52c5edb5768c4c5a3e76f30d6
Aleksey Abramov <capit@LAPTOP-HCOMIG65>
2018-01-30 16:06:43 +0300

    выполнены доп. задания

--- less3.php
+++ less3.php
@@ -43,7 +43,7 @@ foreach ($a as $continent => $animals) {
     foreach ($exploded as $animal) {
         $word_summ = str_word_count($animal);
         if ($word_summ == 2) {
-            $new_a[] = "$animal";
+            $new_a[] = "$continent". " $animal";
         }
     }
 }
@@ -51,13 +51,24 @@ echo '<pre>';
 print_r($new_a);
 foreach ($new_a as $v) {
     $word = str_word_count($v, 1);
-    $descript[] = $word[0];
-    $title[] = $word[1];
+    if (count($word) == 3) {
+    $descript[] = "$word[0]". " $word[1]";
+    $title[] = $word[2];}
+ else {
+    $descript[] = "$word[0]". " $word[1]". " $word[2]" ;
+    $title[] = $word[3];}   
 }
 shuffle($descript);
 shuffle($title);
 for ($i = 0; $i<count($title); $i++) {
     $fantasy[] = $descript[$i]. " $title[$i]";
+    $word = str_word_count($fantasy[$i], 1);
+    if (count($word) == 3) {
+        echo '<h2>'. "$word[0]". '</h2>';
+        echo "$word[1]". " $word[2],";
+    }
+    else {
+       echo '<h2>'. "$word[0]". " $word[1]". '</h2>';
+       echo "$word[2]". " $word[3],"; 
+    }
 }
-echo '<pre>';
-print_r($fantasy);

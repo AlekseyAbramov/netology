a4a39988f8d9bb4db884cf883853418314a90956
Aleksey Abramov <>
2018-02-08 23:01:17 +0300

    первое исправление

---
+++ array.php
@@ -0,0 +1,11 @@
+<?php
+
+//$keys = array_keys($descript);
+//shuffle($keys);
+//$descript1 = [];
+//foreach ($keys as $key) {  
+//    for ($i = 0; $i < count($descript[$key]); $i++) {
+//    $descript1[$key][] = $descript[$key][$i];
+//    }
+//}
+
--- less4.php
+++ less4.php
@@ -1,11 +1,23 @@
 <?php
               
 $url = 'http://api.openweathermap.org/data/2.5/weather?q=Yaroslavl,RU&units=metric&APPID=f856e30cf2d2e0f1c7ef6d82b29cd68a';
-$open = file_get_contents($url);
-$json = json_decode($open);
+$time = time() - 3600;
+$file = 'weather.json';
+
+if (filemtime($file) < $time){
+    $open = file_get_contents($url);
+    $current = file_put_contents($file, $open);
+}
+
+$weather = file_get_contents($file);
+$json = json_decode($weather);
+$temp = $json->main->temp;
+$pressure = $json->main->pressure;
+$humidity = $json->main->humidity;
+$speed = $json->wind->speed;
 
 echo "<img src='http://openweathermap.org/img/w/". $json->weather[0]->icon. ".png'>";
-echo $json->name."<br> температура ". $json->main->temp. "&#176; C";
-echo "<br> давление ". 0.75*$json->main->pressure. " мм рт. ст.";
-echo "<br> влажность ". $json->main->humidity. " %";
-echo "<br> скорость ветра ". $json->wind->speed. " м/с";
+echo $json->name."<br> температура ". $temp. "&#176; C";
+echo "<br> давление ". 0.75*$pressure. " мм рт. ст.";
+echo "<br> влажность ". $humidity. " %";
+echo "<br> скорость ветра ". $speed. " м/с";
